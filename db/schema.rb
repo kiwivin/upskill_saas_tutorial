47e84cbd61743251c655e6d0148fcc924ae880e3
Vinicus Rucoe <v0reynolds@hotmail.co.uk>
2020-11-03 08:37:06 +0000

    Added profiles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201027092228) do
+ActiveRecord::Schema.define(version: 20201103083340) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20201027092228) do
   create_table "plans", force: :cascade do |t|
     t.string  "name"
     t.decimal "price"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id"
+    t.string  "first_name"
+    t.string  "last_name"
+    t.string  "job_title"
+    t.string  "phone_number"
+    t.string  "contact_email"
+    t.text    "description"
   end
 
   create_table "users", force: :cascade do |t|
