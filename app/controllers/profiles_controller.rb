430862dc63c86aed245a4ef30d69121d330e4d0f
Vinicus Rucoe <v0reynolds@hotmail.co.uk>
2020-11-15 07:51:49 +0000

    Added before filters to check current user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
 
   # GET to user/:user_id/profile/new
   def new
@@ -14,7 +16,7 @@ class ProfilesController < ApplicationController
     @profile = @user.build_profile( profile_params)
     if @profile.save
       flash[:success] = "Profile updated!"
-      redirect_to user_path( params[:user_id] )
+      redirect_to user_path(id: params[:user_id] )
     else 
       render action: :new
     end
@@ -26,9 +28,31 @@ class ProfilesController < ApplicationController
     @profile = @user.profile
   end
   
+  # PUT or PATCH to  /users/:user_id/profile
+  def update 
+    # Retrieve user from the database
+    @user = User.find( params[:user_id])
+    # Retrieve that users profile
+    @profile = @user.profile
+    # Mass assign edited profile attributes and save
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile updated!"
+      # Redirect user to their profile page
+      redirect_to user_path(id: params[:user_id])
+    else
+      render action: :edit
+    end
+  end
+    
+  
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title,
       :phone_number, :contact_email, :description)
+    end
+    
+    def only_current_user
+      @user = User.find( params[:user_id])
+      redirect_to(root_url) unless @user == current_user
     end
 end
